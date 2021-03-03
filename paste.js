97beca7941b9a4b46db79083a79e2338484bf6ac
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2021-03-03 12:16:32 +0900

    Create paste.js

---
+++ paste.js
@@ -0,0 +1,4 @@
+//paste this code into the ebs console
+`${getCookie("memberSeq")}|${getCookie("access")}|${getParam("lctreLrnSqno")}|${"100"}`
+//it will show you a long string. paste it into the program.
+//if you want to use a different progress, modify the "100" to a number you want
