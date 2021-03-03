8060dff5964be23096f5178534b67db44022d02b
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2021-03-03 14:29:22 +0900

    Update paste.js

--- paste.js
+++ paste.js
@@ -1,31 +1,4 @@
 //paste this code into the ebs console
-function getCookie(cookieName){
-    var cookieValue=null;
-    if(document.cookie){
-      var array=document.cookie.split((escape(cookieName)+'='));
-      if(array.length >= 2){
-        var arraySub=array[1].split(';');
-        cookieValue=unescape(arraySub[0]);
-      }
-    }
-    return cookieValue;
-}
-function getParam(sname) {
-            var params = location.search.substr(location.search.indexOf("?") + 1);
-            var sval = "";
-
-            params = params.split("&");
-
-            for (var i = 0; i < params.length; i++) {
-                temp = params[i].split("=");
-                if ([temp[0]] == sname) { sval = temp[1]; }
-            }
-            if( sval ) {
-                return decodeURIComponent(sval);
-            } else {
-                return sval;
-            }
-        }
 
 `${getCookie("memberSeq")}|${getCookie("access")}|${getParam("lctreLrnSqno")}|${"100"}`
 
