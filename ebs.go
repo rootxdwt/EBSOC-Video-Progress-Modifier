2dc9b4ab0c37fbe1d4fa249b271a4b3697242e06
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2021-03-05 22:29:00 +0900

    simplified code, now supporting dynamic subdomain

--- ebs.go
+++ ebs.go
@@ -16,19 +16,14 @@ import (
 
 func main() {
 	//important variables
-	var progress int
-	var token, userid, videoid string
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter text: ")
 	text, _ := reader.ReadString('\n')
 	text2 := strings.Split(strings.TrimSpace(text), "|")
-	userid = text2[0]
-	token = text2[1]
-	videoid = text2[2]
-	progress, _ = strconv.Atoi(text2[3])
+	progress, _ := strconv.Atoi(text2[3])
 
 	for i := 0; progress >= i; i++ {
-		formatted := []byte(userid + "|" + videoid + "|" + strconv.Itoa(i))
+		formatted := []byte(text2[0] + "|" + text2[2] + "|" + strconv.Itoa(i))
 
 		for (len(formatted) % 16) != 0 {
 			formatted = append(formatted, 0x00)
@@ -45,8 +40,8 @@ func main() {
 		s := bytes.NewBuffer(q)
 
 		client := &http.Client{}
-		req, _ := http.NewRequest("POST", "https://"+text2[4]+".ebsoc.co.kr/lecture/api/v1/student/learning/"+videoid+"/progress", s)
-		req.Header.Add("X-AUTH-TOKEN", token)
+		req, _ := http.NewRequest("POST", "https://"+text2[4]+".ebsoc.co.kr/lecture/api/v1/student/learning/"+text2[2]+"/progress", s)
+		req.Header.Add("X-AUTH-TOKEN", text2[1])
 		req.Header.Add("Content-Type", "application/json;charset=UTF-8")
 		resp, _ := client.Do(req)
 		f, _ := ioutil.ReadAll(resp.Body)
